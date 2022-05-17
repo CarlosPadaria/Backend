b6ce0f68c5499a9cf731a68921364073d697f2de
Felipe Malicheski <felipemalicheski@gmail.com>
2022-05-17 18:17:25 -0300

    adicionado o metodo post para login

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const { getRepository, Repository } = require('typeorm');
+const express = require("express");
+const { getRepository, Repository } = require("typeorm");
 /*
     Caso dar erro, rodar no terminal:
         yarn add reflect-metadata
@@ -8,37 +8,28 @@ const { getRepository, Repository } = require('typeorm');
 
 */
 const database = require("./src/database");
-const Usuario = require('./src/entity/Usuario');
-database()
+const Usuario = require("./src/entity/Usuario");
+database();
 
 let app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.get("/", (req, res) =>{
-    //req.body
-    if(true)
-    {
-         return res.status(200).json({mensagem: "Hello, World"})
-    }
-    else
-    {
-        return res.status(404).json({status:"Falha"})
-    }
-   
-})
-app.get("/usuarios", async (req, res) =>{
-    //req.body
-    let userRepository = getRepository("Usuario");
-    
+app.get("/", (req, res) => {
+  //req.body
+  if (true) {
+    return res.status(200).json({ mensagem: "Hello, World" });
+  } else {
+    return res.status(404).json({ status: "Falha" });
+  }
+});
+app.get("/usuarios", async (req, res) => {
+  //req.body
+  let userRepository = getRepository("Usuario");
 
-    const usuario = await userRepository.find();
-
-    return res.status(200).json(usuario)
-
-})
-
-
+  const usuario = await userRepository.find();
+  return res.status(200).json(usuario);
+});
 
 /*app.put("/usuarios/:ID_USUARIO", async (req, res) =>{
     const{QUANTIDADE, NOME} = req.body
@@ -54,67 +45,62 @@ app.get("/usuarios", async (req, res) =>{
     
 })*/
 
-app.delete("/usuarios/:ID_USUARIO", async(req, res) => {
+app.post("/login", async (req, res) => {
+  const { EMAIL } = req.body;
+  let userRepository = getRepository("Usuario");
 
+  const usuario = await userRepository.findOne({
+    where: { EMAIL: EMAIL },
+  });
 
-    let userRepository = getRepository("Usuario")
+  return res.status(200).json(usuario);
+});
 
-    req.body =  await userRepository.findOne({
-        where: [
-            {IDPRODUTO: req.params.ID_USUARIO}
-        ]
-    })
-    let DeletarUsuario = await productRepository.delete({
-       ID_USUARIO: `${req.params.ID_USUARIO}`
-    })
+app.delete("/usuarios/:ID_USUARIO", async (req, res) => {
+  let userRepository = getRepository("Usuario");
 
-    if(req.body === null)
-    {
-        return res.status(400).json({message:"Usuário não encontrado"});
-    }
-    else
-    {
-        return res.status(200).json(req.body);
-    }
-})
+  req.body = await userRepository.findOne({
+    where: [{ IDPRODUTO: req.params.ID_USUARIO }],
+  });
+  let DeletarUsuario = await productRepository.delete({
+    ID_USUARIO: `${req.params.ID_USUARIO}`,
+  });
 
-app.post("/usuarios", async(req, res) => {
-    const{NOME, EMAIL, SENHA} = req.body
-    //console.log(req.body)
-    
-    //return res.json({EMAIL, SENHA, NOME});
-    let userRepository = getRepository("Usuario")
+  if (req.body === null) {
+    return res.status(400).json({ message: "Usuário não encontrado" });
+  } else {
+    return res.status(200).json(req.body);
+  }
+});
 
+app.post("/usuarios", async (req, res) => {
+  const { NOME, EMAIL, SENHA } = req.body;
+  //console.log(req.body)
 
-    const usuario = await userRepository.insert(
-        req.body
-    );
+  //return res.json({EMAIL, SENHA, NOME});
+  let userRepository = getRepository("Usuario");
 
-    return res.status(200).json(req.body)
-    
-    
-})
+  const usuario = await userRepository.insert(req.body);
 
-app.get("/usuarios/:ID_USUARIO", async (req, res) =>{
-    console.log(req.params);
-    
-    let userRepository = getRepository("Usuario")
+  return res.status(200).json(req.body);
+});
 
-    const usuario = await userRepository.findOne({
-        where: [
-            {ID_USUARIO: req.params.ID_USUARIO}
-        ]
-    })
-   
-    if (usuario === null)
-    {
-        return res.status(400).json({message:"Usuário não encontrado!"})
-    }
-    else
-    {
-        return res.status(200).json(usuario)
-    }
-})
+app.get("/usuarios/:ID_USUARIO", async (req, res) => {
+  console.log(req.params);
+
+  console.log(req.body);
+  let userRepository = getRepository("Usuario");
+
+  const usuario = await userRepository.findOne({
+    where: [{ ID_USUARIO: req.params.ID_USUARIO }],
+  });
+
+  if (usuario === null) {
+    return res.status(400).json({ message: "Usuário não encontrado!" });
+  } else {
+    return res.status(200).json(usuario);
+  }
+});
 
 /*app.patch("/users/:IDPRODUTO", async (req, res) =>{
     console.log(req.params);
@@ -142,5 +128,5 @@ app.get("/usuarios/:ID_USUARIO", async (req, res) =>{
 })*/
 
 app.listen(3333, () => {
-    console.log("mensagem fofa >-<")
-})
+  console.log("mensagem fofa >-<");
+});
